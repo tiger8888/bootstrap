f43d4fdbc15c40a457c8635a2b67251d3a3b4fa4
guanzhisong <guanzhisong@jiashuangkuaizi.com>
2016-06-15 11:38:07 +0800

    add threadpool test

---
+++ src/test/java/com/gzs/learn/bootstrap/TestCachedThreadPool.java
@@ -0,0 +1,44 @@
+package com.gzs.learn.bootstrap;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+public class TestCachedThreadPool {
+    @Test
+    public void testCachedPool() throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        /*
+         * ExecutorService executorService = new ThreadPoolExecutor(10, 200, 60, TimeUnit.SECONDS, new
+         * SynchronousQueue<Runnable>());
+         */
+        /*
+         * ExecutorService executorService = new ThreadPoolExecutor(100, 200, 60, TimeUnit.SECONDS, new
+         * LinkedBlockingQueue<Runnable>());
+         */
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 300000; i++) {
+            executorService.submit(() -> {
+                try {
+                    Thread.sleep(1 * 100);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(30, TimeUnit.SECONDS);
+        long cost = System.currentTimeMillis() - start;
+        System.out.println("cost is:" + cost);
+    }
+}
+
+class SimpleThreadFactory implements ThreadFactory {
+    @Override
+    public Thread newThread(Runnable r) {
+        return null;
+    }
+}
